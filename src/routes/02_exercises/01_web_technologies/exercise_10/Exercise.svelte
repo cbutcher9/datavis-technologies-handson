7c28076cadc78d367e86dfad100c88a89d890169
cbutcher9 <clkbutcher@btinternet.com>
2023-03-20 17:48:05 +0100

    10

---
+++ src/routes/02_exercises/01_web_technologies/exercise_10/Exercise.svelte
@@ -0,0 +1,8 @@
+<script> import Exercise2 from '/home/cbutcher9/datavis-technologies-handson/src/routes/02_exercises/01_web_technologies/exercise_02/Exercise.svelte';
+         import Exercise3 from '/home/cbutcher9/datavis-technologies-handson/src/routes/02_exercises/01_web_technologies/exercise_03/Exercise.svelte';
+</script>
+
+
+<div> <Exercise3> </Exercise3>
+    <Exercise2></Exercise2>
+</div>
