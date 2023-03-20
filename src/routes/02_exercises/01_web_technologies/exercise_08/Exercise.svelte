baca1db456f7b3c2bbc6d1998e60ceb05ee827a2
cbutcher9 <clkbutcher@btinternet.com>
2023-03-20 14:46:45 +0100

    8

---
+++ src/routes/02_exercises/01_web_technologies/exercise_08/Exercise.svelte
@@ -0,0 +1,7 @@
+<script> const student = {name: ' Chris Butcher',
+    things:['data','graphs','plots']}
+</script>
+
+<p> My name is {student['name']} and these are some of 
+    my favourite things {student['things']}
+</p>
